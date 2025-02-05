a99d166bbbd136bbdc1d3e891e132f69cdb50358
Rayvel M. Taruc <rayvelm.taruc02@gmail.com>
2025-02-05 10:36:59 +0800

    Adding Web Project 20 pages and Site 2
    Testing out SubFolder Hosting in git pages

---
+++ Project 20 pages/functionality/script.js
@@ -0,0 +1,53 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
+
+$(document).ready(function(){
+    $('.slider').slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
+      nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  });
